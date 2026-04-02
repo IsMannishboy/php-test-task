8fc164af35824da1fc43ce879c1c18594a862aa2
21savage <kkravcuk334@gmail.com>
2026-04-02 22:58:27 +0300

    removed php-task folder

--- php-task/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TicketController;
-Route::get('/tickets', [TicketController::class, 'index']);
-Route::post('/tickets', [TicketController::class, 'store']);
--- php-task/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/widget', function () {
-    return view('widget');
-});
